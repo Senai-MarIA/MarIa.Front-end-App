6cde70d0c829392632a1ede0218870853424497e
nauan2024 <nauanlaurentinotec@gmail.com>
2026-03-18 00:00:20 -0300

    feat:organizate the html for main

--- MarIa-App/src/App.jsx
+++ MarIa-App/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/layouts/Header"
 import Footer from "./components/layouts/Footer"
+import SectionMain from "./components/layouts/Section-Main"
 function App() {
     return(
         <>
         <Header/>
+        <SectionMain/>
         <Footer/>
         </>
     )
--- MarIa-App/src/components/layouts/Section-Main/index.jsx
+++ MarIa-App/src/components/layouts/Section-Main/index.jsx
@@ -1,15 +1,31 @@
 import  './style.css'
 
-function Header() {
+function Main() {
     return (
         <>
 
 
             <section className="Section_Main">
-                <span>Nunca mais perca o dia da coleta!</span>
-                <h1>Descarte inteligente para uma cidade mais limpa.</h1>
-                <p>Descubra os dias exatos da coleta seletiva e comum na sua rua, encontre pontos de descarte para lixo eletrônico e das operações cata-bagulho com facilidade.</p>
+                <span>
+                    <img src="../../../assets/img/IconSino.svg" alt="" />
+                   <p> Nunca mais perca o dia da coleta!</p>
+                   </span>
+                   <div className="container1main">
+                        <div className="group1Main">
+                            <h1>Descarte inteligente para uma <span>cidade mais limpa.</span></h1>
+                              <p>Descubra os dias exatos da coleta seletiva e comum na sua rua, encontre pontos de descarte para lixo eletrônico e das operações cata-bagulho com facilidade.</p>
+                         </div>
+                         <div className="buttonMain">
+                            <a href="">Baixar Gratuitamente</a>
+                            <a href="">Veja como funciona</a>
+                        </div>
+                </div>
+                <div className="container2main">
+                    <img src="../../../assets/img/IconCell.svg" alt="" />
+                    /*nao ta funcionando a importação da imagem, por isso coloquei o caminho direto, arrumar depois*/
+                </div>
             </section>
         </>
     )
 }
+export default Main;
