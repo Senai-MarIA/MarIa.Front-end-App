52843b50bac518539479486b6c0c2e17ee609289
nauan2024 <nauanlaurentinotec@gmail.com>
2026-03-17 23:40:28 -0300

    feat:initiate the main

---
+++ MarIa-App/src/components/layouts/Section-Main/index.jsx
@@ -0,0 +1,15 @@
+import  './style.css'
+
+function Header() {
+    return (
+        <>
+
+
+            <section className="Section_Main">
+                <span>Nunca mais perca o dia da coleta!</span>
+                <h1>Descarte inteligente para uma cidade mais limpa.</h1>
+                <p>Descubra os dias exatos da coleta seletiva e comum na sua rua, encontre pontos de descarte para lixo eletrônico e das operações cata-bagulho com facilidade.</p>
+            </section>
+        </>
+    )
+}
