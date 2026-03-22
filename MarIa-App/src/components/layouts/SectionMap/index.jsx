dae40e867201a8d324046e4e69a187516e51bad5
nauan2024 <nauanlaurentinotec@gmail.com>
2026-03-22 20:20:43 -0300

    feat:organizate the section Map

--- MarIa-App/src/components/layouts/SectionMap/index.jsx
+++ MarIa-App/src/components/layouts/SectionMap/index.jsx
@@ -2,6 +2,7 @@ import iconmap from "../../../assets/img/Map.svg"
 import SearchIcon from "../../../assets/img/SearchIcon.svg"
 import caminhaoIcon from "../../../assets/img/caminhao.svg"
 import iconOrangeCaminhao from "../../../assets/img/iconOrangeCaminhao.svg"
+import IconEnergy from "../../../assets/img/IconEnergy.svg"
 export default function SectionMap() {
     return (
         <section className="section-map">
@@ -10,9 +11,11 @@ export default function SectionMap() {
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4162.476370376381!2d-46.47681158860001!3d-23.545262378722455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce66dec98fb855%3A0xf2b061ffbcd2ecf8!2sNeo%20Qu%C3%ADmica%20Arena!5e1!3m2!1spt-BR!2sbr!4v1774220575295!5m2!1spt-BR!2sbr" width="584" height="736"  allowfullscreen="" loading="lazy"></iframe>
             
             <div className="complementationMap">
-                <span><img src={iconOrangeCaminhao} alt="" /></span>
-                <span><img src={iconOrangeCaminhao} alt="" /></span>
-
+                <span><img src={iconOrangeCaminhao} alt="" />Cata-Bagulho</span>
+                <span><img src={IconEnergy} alt="" />Lixo Eletrônico</span>
+                <h3>Endereço:</h3>
+                <p className="local">Estádio do cortinas melhor time do brasil</p>
+                <p className="Bairro-cidade">Itaquera, São Paulo</p>
             </div>
             </div>
             <div className="group2map">
