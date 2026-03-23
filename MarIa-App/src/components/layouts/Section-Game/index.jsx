688acecfd1704077d4b3cb984971063d7fced76b
nauan2024 <nauanlaurentinotec@gmail.com>
2026-03-23 00:00:14 -0300

    feat: finished the scope off project

--- MarIa-App/src/components/layouts/Section-Game/index.jsx
+++ MarIa-App/src/components/layouts/Section-Game/index.jsx
@@ -13,12 +13,24 @@ function SectionGame() {
 
             
             <p className="PHero">Torne-se um Herói da Natureza!</p>
-            <p>Aprender a reciclar nunca foi tão divertido. Ajude nossa coruja mascote, a MarIA, a colocar todo o lixo nas lixeiras corretas e ganhe pontos ecológicos. Um jogo perfeito para ensinar as crianças sobre sustentabilidade.</p>
+            <p className="DescriptionGame">Aprender a reciclar nunca foi tão divertido. Ajude nossa coruja mascote, a MarIA, a colocar todo o lixo nas lixeiras corretas e ganhe pontos ecológicos. Um jogo perfeito para ensinar as crianças sobre sustentabilidade.</p>
             <button><img src={IconControl} alt="" />Jogue Agora</button>
           </div>
           <div className="GroupImageGame">
             <img src={MarIAWithItems} alt="" />
           </div>
+          <svg className="Waveone" width="1280" height="116" viewBox="0 0 1280 116" fill="none" xmlns="http://www.w3.org/2000/svg"         preserveAspectRatio="none"
+>
+<path d="M436 112.5C436 112.5 12.081 115.791 -17.9289 115.791C-47.9388 115.791 254.161 0.755739 499.659 0.232461C745.157 -0.290816 1280 0.232461 1280 0.232461C1280 0.232461 1213.13 38.2066 897.699 84.3196C582.269 130.433 436 112.5 436 112.5Z" fill="#B742FF"/>
+
+</svg>
+
+<svg className="Wavetwo" width="696" height="197" viewBox="0 0 696 197" fill="none" xmlns="http://www.w3.org/2000/svg"         preserveAspectRatio="none"
+>
+<path d="M706.268 0L730 197H0C0 197 57.3987 126.5 357.638 98C657.877 69.5 706.268 0 706.268 0Z" fill="#A21EF4"/>
+</svg>
+
+
         </section>
     );
 }
