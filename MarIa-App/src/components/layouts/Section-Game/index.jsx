01c0c18051e1e109a96a44c6f15821a0465879f4
nauan2024 <nauanlaurentinotec@gmail.com>
2026-03-22 23:34:54 -0300

    style:button in the section-Game

--- MarIa-App/src/components/layouts/Section-Game/index.jsx
+++ MarIa-App/src/components/layouts/Section-Game/index.jsx
@@ -1,15 +1,20 @@
 import  './style.css'
 import MarIAWithItems from "../../../assets/img/MarIAwithItems.svg"
 import LogoTipoGame from "../../../assets/img/LogoTipoGame.svg"
+import IconControl from "../../../assets/img/IconControl.svg"
 function SectionGame() {
     return (
         <section className="sectiongame">
           <div className="GroupContentGame">
+            <div className="groupmission">
             <h2>Missão</h2>
+            <img src={LogoTipoGame} alt="" />
+            </div>
+
+            
             <p>Torne-se um Herói da Natureza!</p>
             <p>Aprender a reciclar nunca foi tão divertido. Ajude nossa coruja mascote, a MarIA, a colocar todo o lixo nas lixeiras corretas e ganhe pontos ecológicos. Um jogo perfeito para ensinar as crianças sobre sustentabilidade.</p>
-            <img src={LogoTipoGame} alt="" />
-            <button><img src={LogoTipoGame} alt="" />Jogue Agora</button>
+            <button><img src={IconControl} alt="" />Jogue Agora</button>
           </div>
           <div className="GroupImageGame">
             <img src={MarIAWithItems} alt="" />
