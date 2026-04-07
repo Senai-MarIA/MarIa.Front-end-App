14dee027c8e3882258bf202cbfad48e304539b3b
nauan2024 <nauanlaurentinotec@gmail.com>
2026-04-07 16:15:37 -0300

    header fixed

--- MarIa-App/src/components/layouts/Header/index.jsx
+++ MarIa-App/src/components/layouts/Header/index.jsx
@@ -1,19 +1,41 @@
 import MarIa_Logo from "../../../assets/img/MariaLogo.svg"
 import downLoadIcon from "../../../assets/img/iconDownload.svg"
+import { useState } from "react"
 import  './style.css'
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
     return (
         <>
-            <section className="Header_section">
+        <section className="containerHeader">
+            <article className="Header_section">
+                
                 <img src={MarIa_Logo} />
-                <ul>
+                
+               
+                <button className="Download_Button">
+                    <img src={downLoadIcon} />
+                    Baixar App
+                </button>
+                  <div className="organizatorHeader"> 
+                <button className={`Menu_Toggle ${isMenuOpen ? 'active' : ''}`} 
+                    onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+               
+                <ul className={isMenuOpen ? "active" : ""}>
                     <li><a href="">Funcionalidades</a></li>
                     <li><a href="">Busque por Cep</a></li>
                     <li><a href="">Mapa de Descarte</a></li>
                     <li><a href="">Missão MarIa</a></li>
                 </ul>
-                <button>Baixar App <img src={downLoadIcon}/></button>
+                </div>
+            </article>
             </section>
         </>
     )
